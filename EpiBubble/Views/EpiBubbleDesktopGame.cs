0a0c87d63de303a3d3a9a22cf41fd5addf04c351
damien tohin <dtohin@outlook.fr>
2019-01-08 18:40:02 +0100

    Simple additions

--- EpiBubble/Views/EpiBubbleDesktopGame.cs
+++ EpiBubble/Views/EpiBubbleDesktopGame.cs
@@ -10,6 +10,7 @@ namespace EpiBubble
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        private Texture2D _pointer;
 
         public EpiBubbleDesktopGame()
         {
@@ -38,6 +39,7 @@ namespace EpiBubble
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            _pointer = Content.Load<Texture2D>("Pointer");
 
             // TODO: use this.Content to load your game content here
         }
@@ -70,7 +72,11 @@ namespace EpiBubble
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            spriteBatch.Begin();
+            var yposition = graphics.GraphicsDevice.Viewport.Height - _pointer.Height - 10;
+            var xposition = (graphics.GraphicsDevice.Viewport.Width / 2) - (_pointer.Width / 2);
+            spriteBatch.Draw(_pointer, new Vector2(xposition, yposition), Color.White);
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
