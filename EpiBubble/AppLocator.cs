ace707d6ae56fb9f26e08a2bb210062fc5715144
damien tohin <dtohin@outlook.fr>
2019-01-20 10:43:39 +0100

    Created and registered SaveViewmodel

--- EpiBubble/AppLocator.cs
+++ EpiBubble/AppLocator.cs
@@ -1,4 +1,5 @@
-﻿using EpiBubble.Services;
+﻿using EpiBubble.Model.DataModel;
+using EpiBubble.Services;
 using EpiBubble.ViewModels;
 using Splat;
 using System;
@@ -14,12 +15,14 @@ namespace EpiBubble
         static public SetupViewModel SetupViewModel => Locator.CurrentMutable.GetService<SetupViewModel>();
         static public SaveViewModel SaveViewModel => Locator.CurrentMutable.GetService<SaveViewModel>();
         static public GamePageViewModel GamePageViewModel => Locator.CurrentMutable.GetService<GamePageViewModel>();
+        static public IDataService<UserOptions> DataService => Locator.CurrentMutable.GetService<IDataService<UserOptions>>();
 
         public AppLocator()
         {
+            Locator.CurrentMutable.Register<IDataService<UserOptions>>(() => new UserOptionsDataService());
             Locator.CurrentMutable.Register(() => new SimpleNavigationService());
             Locator.CurrentMutable.Register(() => new SetupViewModel());
-            Locator.CurrentMutable.Register(() => new SaveViewModel());
+            Locator.CurrentMutable.Register(() => new SaveViewModel(DataService));
             Locator.CurrentMutable.Register(() => new GamePageViewModel(Locator.CurrentMutable.GetService<SimpleNavigationService>()));
         }
     }
--- EpiBubble/ViewModels/SaveViewModel.cs
+++ EpiBubble/ViewModels/SaveViewModel.cs
@@ -1,4 +1,6 @@
-﻿using ReactiveUI;
+﻿using EpiBubble.Model.DataModel;
+using EpiBubble.Services;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,20 @@ namespace EpiBubble.ViewModels
 {
     public class SaveViewModel : ReactiveObject
     {
+        string _userName;
+        public string UserName
+        {
+            get => _userName;
+            set => this.RaiseAndSetIfChanged(ref _userName, value);
+        }
+
+        public UserOptions UserOptions { get; set; }
+
+        IDataService<UserOptions> _dataService;
+
+        public SaveViewModel(IDataService<UserOptions> dataService)
+        {
+            _dataService = dataService;
+        }
     }
 }
